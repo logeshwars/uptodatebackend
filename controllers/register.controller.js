1c14b87da047c55e1e81551f2721350907fbaa70
logeshwars <stylishstarlogesh15@gmail.com>
2022-03-09 13:49:34 +0530

    register updated

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -5,12 +5,14 @@ export const register = (req, res) => {
   eventdb.updateOne(
     { _id: data.eid },
     { $push: { registers: data.uid } },
+    { safe: true, upsert: true },
     (err, data) => {
       if (err) res.status(500).send("Registeration Unsuccessful");
       else {
         userdb.updateOne(
           { _id: data.uid },
           { $push: { events: data.eid } },
+          { safe: true, upsert: true },
           (err, data) => {
             if (err) res.status(500).send(err);
             else
