9e8c91847884e4ea1f6a3917fc4b4c3dec4604a8
logeshwars <stylishstarlogesh15@gmail.com>
2022-03-09 10:59:32 +0530

    register updated

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,19 +1,22 @@
 import userdb from "../models/users.js";
 import eventdb from "../models/events.js";
-export const createUser = (req, res) => {
+export const register = (req, res) => {
   const data = req.body;
   eventdb.updateOne(
     { _id: data.eid },
     { $push: { registers: data.uid } },
     (err, data) => {
-      if (err) res.status(500).send(err);
+      if (err) res.status(500).send("Registeration Unsuccessful");
       else {
         userdb.updateOne(
           { _id: data.uid },
           { $push: { events: data.eid } },
           (err, data) => {
             if (err) res.status(500).send(err);
-            else res.status(201).send({ status: "created", data: data });
+            else
+              res
+                .status(201)
+                .send({ status: "Registered succesfully", data: data });
           }
         );
       }
--- router/event.router.js
+++ router/event.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import { createEvent } from "../controllers/createevent.controller.js";
 import { getEvent } from "../controllers/getevent.controller.js";
 import { getEventById } from "../controllers/geteventbyid.controller.js";
+import { register } from "../controllers/register.controller.js";
 router.post("/createevent", createEvent);
 router.get("/getevent", getEvent);
 router.get("/geteventbyid", getEventById);
