67616249954cd9f57281cae26850aae71b58402a
logeshwars <stylishstarlogesh15@gmail.com>
2022-03-09 14:02:59 +0530

    register updated

--- router/user.js
+++ router/user.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { createUser } from "../controllers/createuser.controller.js";
 import { getUser } from "../controllers/getuser.controller.js";
-import { getEventById } from "../controllers/geteventbyid.controller.js";
+import { getUserById } from "../controllers/getuserbyid.controller.js";
 const router = express.Router();
 router.post("/register", createUser);
 router.get("/getuser", getUser);
-router.get("/getuserbyid", getEventById);
+router.get("/getuserbyid", getUserById);
 export default router;
