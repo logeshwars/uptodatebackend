2d1c1cac5ace0408721c171ffd3b4c8d7a9dd1cc
logeshwars <stylishstarlogesh15@gmail.com>
2022-03-18 21:06:25 +0530

    get hall updated

--- controllers/getavailhall.controller.js
+++ controllers/getavailhall.controller.js
@@ -1,6 +1,7 @@
 import halldb from "../models/halls.js";
 export const getAvailHall = (req, res) => {
-  halldb.find({ $lt: { bookedDate: new Date.now() } }, (err, data) => {
+  console.log(new Date());
+  halldb.find({ bookedDate: { $lt: new Date() } }, (err, data) => {
     if (err) res.status(500).send(err);
     else {
       if (data.length > 0) res.status(201).send(data);
